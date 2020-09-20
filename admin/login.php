8572870e03eb3e05f44f5ac768c207a09a7338f7
2yman <ayman_20140114@fci.helwan.edu.eg>
2020-09-20 18:13:31 +0200

    clean login page

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,4 @@
-<?php include("includes/header.php") ?>
+<?php include("includes/init.php") ?>
 
 <?php if ($session->is_SignedIn()) {redirect("index.php");} ?>
 
@@ -30,27 +30,57 @@ if ($userfound) {
 
 
 
-<div class="container">
-    <div class="row justify-content-center align-items-center" style="height:100vh">
-        <div class="col-4">
-            <div class="card">
-            <h4 class="bg-danger"><?php echo $msg ?> </h4>
-                <div class="card-body">
-                    <form action="" autocomplete="off" method="post">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" class="form-control" name="username" value="<?php echo $username ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">password</label>
-                            <input type="password" class="form-control" name="password">
-                        </div>
-                        <button type="submit" id="sendlogin" class="btn btn-primary" name="submit" value="submit">login</button>
-                    </form>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
+            <div class="col-4">
+                <div class="card">
+                    <h4 class="bg-danger"><?php echo $msg ?> </h4>
+                    <div class="card-body">
+                        <form action="" autocomplete="off" method="post">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" name="username" value="<?php echo $username ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">password</label>
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                            <button type="submit" id="sendlogin" class="btn btn-primary" name="submit"
+                                value="submit">login</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+</body>
 
-<?php include("includes/footer.php"); ?>
+</html>
